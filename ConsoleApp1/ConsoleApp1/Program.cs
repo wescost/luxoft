b6194b3671851eeedcff0e56074e386481e2e5a7
Vladimir Ganich <wescost@ukr.net>
2017-07-27 08:39:13 +0300

    console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"Data\data.txt");
+            var wordsList = text.Split(new Char[] { ' ', ';', '\"', ',',':' }, StringSplitOptions.RemoveEmptyEntries);
+
+            using (StreamWriter writetext = new StreamWriter(@"Data\result.txt"))
+            {
+                var y = 0;
+                foreach (var word in wordsList)
+                {
+                    if (word != "*")
+                    {
+                        int counter = 0;
+                        writetext.Write(word + " ");
+                        for (int i = y; i < wordsList.Length; i++)
+                        {
+                            if (word == wordsList[i])
+                            {
+                                counter++;
+                                wordsList[i] = "*";
+                            }
+                        }
+                        writetext.WriteLine(counter);
+                        y++;
+                    }
+
+                }
+            }
+        }
+    }
+}
