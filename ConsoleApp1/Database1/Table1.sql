835c2c6fe47e2029d6ab56f5e645581542116768
Vladimir Ganich <wescost@ukr.net>
2017-07-27 09:34:02 +0300

    table

---
+++ ConsoleApp1/Database1/Table1.sql
@@ -0,0 +1,16 @@
+﻿/*
+The database must have a MEMORY_OPTIMIZED_DATA filegroup
+before the memory optimized object can be created.
+
+The bucket count should be set to about two times the 
+maximum expected number of distinct values in the 
+index key, rounded up to the nearest power of two.
+*/
+
+CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY NONCLUSTERED HASH WITH (BUCKET_COUNT = 131072), 
+    [Name] NCHAR(10) NULL, 
+    [Region] NCHAR(10) NULL, 
+    [Supplier] NCHAR(10) NULL
+) WITH (MEMORY_OPTIMIZED = ON)
